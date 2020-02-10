9833fbd8b3ee72734709447ebf756594a82b7810
timbotimber <hitimstephens@gmail.com>
2020-02-10 11:52:38 +0100

    add email adress to sign up

--- proj2/models/User.js
+++ proj2/models/User.js
@@ -1,13 +1,15 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  favouriteWeAnFilm: String,
-  profession: String
-});
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+    username: String,
+    emailaddress: String
+    password: String,
+    favouriteWeAnFilm: String,
+    profession: String
+  });
+  
+  const User = mongoose.model('User', userSchema);
+  
+  module.exports = User;
