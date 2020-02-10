e0fecc9353e7160b5bae0748cadc0d54f3d0c134
sha-hinya <sha.haeckel@gmail.com>
2020-02-10 14:17:39 +0100

    test git

---
+++ proj2/bin/seeds.js
@@ -0,0 +1,11 @@
+const locations = [
+    {
+        owner: "Anna Smith", //User id
+        placeName: "Makapu’u Point, Oʻahu, Hawaii",
+        builtDate: 1909,
+        description: "The viewfinders found on Makapu’u Point in O’ahu, Hawaii offer unparalleled views into the Pacific Ocean, yet their location on Makapu’u - the easternmost point of the island - can also provide a look into the island’s past.",
+        image: "/images/81814544_122817308991684_5690493190330451422_n",
+        WesAnQuote: String
+    }
+]
+
