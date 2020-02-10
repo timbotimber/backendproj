6447a87106a41c93cec5abbeba1fd439f1f48fef
sha-hinya <sha.haeckel@gmail.com>
2020-02-10 23:37:30 +0100

    add 20 images and create and populate seed file

--- proj2/models/Location.js
+++ proj2/models/Location.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema; 
+const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-    owner: String, //User id
-    placeName: String,
-    builtDate: Number,
-    description: String,
-    image: URL,
-    WesAnQuote: String
-  });
-  
-  const Location = mongoose.model('Location', locationSchema);
-  
-  module.exports = Location;
+  owner: String, //User id
+  image: String,
+  placeName: String,
+  builtDate: Number,
+  located: String,
+  description: String,
+  WesAnQuote: String
+});
+
+const Location = mongoose.model('Location', locationSchema);
+
+module.exports = Location;
--- proj2/routes/index.js
+++ proj2/routes/index.js
@@ -7,3 +7,4 @@ const router = express.Router();
 // });
 
 module.exports = router;
+
