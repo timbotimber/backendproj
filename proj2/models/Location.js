0b8fea803f3b28fe823fb7e3405ac2b3103ac927
timbotimber <hitimstephens@gmail.com>
2020-02-11 12:48:09 +0100

    lat longs

--- proj2/models/Location.js
+++ proj2/models/Location.js
@@ -1,16 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
   owner: String, //User id
   image: String,
   placeName: String,
-  builtDate: Number,
-  located: String,
+  builtData: Date,
   description: String,
-  WesAnQuote: String
+  coordinates: [Number],
+  wesAndersonMovie: {
+    type: String,
+    enum: [
+      "Rushmore",
+      "The Royal Tenenbaums",
+      "The Life Aquatic with Steve Zisso",
+      "The Darjeeling Limited",
+      "Fantastic Mr. Fox",
+      "Moonrise Kingdom",
+      "The Grand Budapest Hotel",
+      "Isle of Dogs"
+    ]
+  }
 });
 
-const Location = mongoose.model('Location', locationSchema);
+const Location = mongoose.model("Location", locationSchema);
 
 module.exports = Location;
--- proj2/routes/locations.js
+++ proj2/routes/locations.js
@@ -5,20 +5,21 @@ const Location = require("../models/Location");
 router.get("/locations", (req, res) => {
   Location.find()
     .then(locations => {
-      res.render("../views/locations/list.hbs", { locations, user: req.user });
+      res.send(require("../data.js"));
+      // res.render("../views/locations/list.hbs", { locations, user: req.user });
     })
     .catch(err => {
       next(err);
     });
 });
 
-// const loginCheck = (req, res, next) => {
-//   if (req.session.user) {
-//     next();
-//   } else {
-//     res.redirect("/");
-//   }
-// };
+const loginCheck = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+};
 
 // router.get(
 //   "/locations/add",
@@ -30,6 +31,18 @@ router.get("/locations", (req, res) => {
 
 router.get("/locations/:id", (req, res, next) => {
   Location.findById(req.params.id);
+});
+
+router.patch("/locations/:id", (req, res, next) => {
+  const changes = req.body; // in our axios call on the front-end, we'll make sure to pass the fields that need to be updated
+  Location.updateOne({ _id: req.params.id }, changes)
+    .then(() => {
+      // successful update, we can send a response
+      res.json();
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
