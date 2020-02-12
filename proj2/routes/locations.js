9a0ae4555f05edad1aaaae5049dc44dcd7030d2e
sha-hinya <sha.haeckel@gmail.com>
2020-02-12 18:47:46 +0100

    xy1

--- proj2/routes/locations.js
+++ proj2/routes/locations.js
@@ -40,68 +40,68 @@ router.post("/locations/add", (req, res) => {
   });
 
   // foto upload
-  router.post("/locations/add", uploadCloud.single("image"), (req, res) => {
-    let name = req.body.placeName;
-    let date = req.body.date;
-    let description = req.body.description;
-    let quote = req.body.quote;
-    let coordinates = req.body.coordinates.split(",");
-    console.log(coordinates);
-    const newLocation = new Location({
-      name,
-      date,
-      description,
-      quote,
-      coordinates
+  // router.post("/locations/add", uploadCloud.single("image"), (req, res) => {
+  //   let name = req.body.placeName;
+  //   let date = req.body.date;
+  //   let description = req.body.description;
+  //   let quote = req.body.quote;
+  //   let coordinates = req.body.coordinates.split(",");
+  //   console.log(coordinates);
+  //   const newLocation = new Location({
+  //     name,
+  //     date,
+  //     description,
+  //     quote,
+  //     coordinates
+  //   });
+
+
+  newLocation
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.log(error);
     });
+});
 
+router.get("/locations/:locationId", (req, res, next) => {
+  const locationsId = req.params.locationId;
+  Location.findById(locationsId)
+    .then(location => {
+      console.log(location);
+      res.render("locations/location.hbs", location);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
-    newLocation
-      .save()
-      .then(() => {
-        res.redirect("/");
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  });
+// router.get("/rawdata/:id", (req, res, next) => {
+//   Location.findById(req.params.id);
+// });
 
-  router.get("/locations/:locationId", (req, res, next) => {
-    const locationsId = req.params.locationId;
-    Location.findById(locationsId)
-      .then(location => {
-        console.log(location);
-        res.render("locations/location.hbs", location);
-      })
-      .catch(err => {
-        next(err);
-      });
-  });
+router.patch("/rawdata/:id", (req, res, next) => {
+  const changes = req.body; // in our axios call on the front-end, we'll make sure to pass the fields that need to be updated
+  Location.updateOne({ _id: req.params.id }, changes)
+    .then(() => {
+      // successful update, we can send a response
+      res.json();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
-  // router.get("/rawdata/:id", (req, res, next) => {
-  //   Location.findById(req.params.id);
-  // });
+router.get("/rawdata", (req, res, next) => {
+  Location.find()
+    .then(locationDocument => {
+      res.json(locationDocument);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
-  router.patch("/rawdata/:id", (req, res, next) => {
-    const changes = req.body; // in our axios call on the front-end, we'll make sure to pass the fields that need to be updated
-    Location.updateOne({ _id: req.params.id }, changes)
-      .then(() => {
-        // successful update, we can send a response
-        res.json();
-      })
-      .catch(err => {
-        next(err);
-      });
-  });
-
-  router.get("/rawdata", (req, res, next) => {
-    Location.find()
-      .then(locationDocument => {
-        res.json(locationDocument);
-      })
-      .catch(err => {
-        next(err);
-      });
-  });
-
-  module.exports = router;
+module.exports = router;
