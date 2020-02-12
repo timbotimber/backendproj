ec453f848c9ad35e14a4a80090bbaf49471d0e20
sha-hinya <sha.haeckel@gmail.com>
2020-02-12 18:44:49 +0100

    linked to map

--- proj2/routes/locations.js
+++ proj2/routes/locations.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Location = require("../models/Location");
+const uploadCloud = require("../config/cloudinary.js");
 // const locationsJS = require("../bin/data");
 
 router.get("/locations", (req, res) => {
@@ -37,52 +38,70 @@ router.post("/locations/add", (req, res) => {
     quote,
     coordinates
   });
-  newLocation
-    .save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(error => {
-      console.log(error);
+
+  // foto upload
+  router.post("/locations/add", uploadCloud.single("image"), (req, res) => {
+    let name = req.body.placeName;
+    let date = req.body.date;
+    let description = req.body.description;
+    let quote = req.body.quote;
+    let coordinates = req.body.coordinates.split(",");
+    console.log(coordinates);
+    const newLocation = new Location({
+      name,
+      date,
+      description,
+      quote,
+      coordinates
     });
-});
 
-router.get("/locations/:locationId", (req, res, next) => {
-  const locationsId = req.params.locationId;
-  Location.findById(locationsId)
-    .then(location => {
-      console.log(location);
-      res.render("locations/location.hbs", location);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
 
-// router.get("/rawdata/:id", (req, res, next) => {
-//   Location.findById(req.params.id);
-// });
+    newLocation
+      .save()
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
 
-router.patch("/rawdata/:id", (req, res, next) => {
-  const changes = req.body; // in our axios call on the front-end, we'll make sure to pass the fields that need to be updated
-  Location.updateOne({ _id: req.params.id }, changes)
-    .then(() => {
-      // successful update, we can send a response
-      res.json();
-    })
-    .catch(err => {
-      next(err);
-    });
-});
+  router.get("/locations/:locationId", (req, res, next) => {
+    const locationsId = req.params.locationId;
+    Location.findById(locationsId)
+      .then(location => {
+        console.log(location);
+        res.render("locations/location.hbs", location);
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
 
-router.get("/rawdata", (req, res, next) => {
-  Location.find()
-    .then(locationDocument => {
-      res.json(locationDocument);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
+  // router.get("/rawdata/:id", (req, res, next) => {
+  //   Location.findById(req.params.id);
+  // });
 
-module.exports = router;
+  router.patch("/rawdata/:id", (req, res, next) => {
+    const changes = req.body; // in our axios call on the front-end, we'll make sure to pass the fields that need to be updated
+    Location.updateOne({ _id: req.params.id }, changes)
+      .then(() => {
+        // successful update, we can send a response
+        res.json();
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+
+  router.get("/rawdata", (req, res, next) => {
+    Location.find()
+      .then(locationDocument => {
+        res.json(locationDocument);
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+
+  module.exports = router;
